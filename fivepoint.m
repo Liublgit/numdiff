9b144c531faee24a9f11627e56c4ad06af3ba398
Mira <miralilleholtvik@gmail.com>
2018-03-20 10:47:03 +0100

    five point formula with solution

--- fivepoint.m
+++ fivepoint.m
@@ -6,15 +6,12 @@ function U =  fivepoint(fval,M,N)
 
 h = 1/(M+1); %distance between nodes in x-dir
 k = 1/(N+1); %distance between nodes in y-dir
-
 %Construct A, the sparse diagonal block matrix diag(I,T,I)
 e = ones(M*N,1);
 f = ones(M*N,1);
 f(M:M:N*M) = 0;
 g = [1;f(1:end-1)];
 A = spdiags([1/k^2*e,1/h^2*f,-2*(1/h^2 + 1/k^2)*e, 1/h^2*g, 1/k^2*e],[-M,-1,0,1,M],M*N,M*N);
-
-U = A\F;
-%U = reshape(Uvec,M,N)';
-
+%solve the linear system to find U
+U = A\fval;
 end
