a389969a7b825e3b7755a69603a388a51f026cb6
sivertsel <18726460+sivertsel@users.noreply.github.com>
2018-03-20 15:44:40 +0100

    error i error

--- error.m
+++ error.m
@@ -1,8 +1,10 @@
 function [err_x, err_y] = error(f,analytic)
-    n = 120;
+    n = 300;
+    
     err_x = zeros(2,8);
     err_y = zeros(2,8);
     
+   
     % Check error/corvergance rate in m-direction
     for p =1:8    
         m= 2^p;
@@ -16,13 +18,13 @@ function [err_x, err_y] = error(f,analytic)
         fval = reshape(f(X,Y),m*n,1);
         u = reshape(analytic(X,Y),m*n,1);
         U = fivepoint(fval,m,n);
-        err_x(2,p) = norm(u-U,2)*sqrt(h*k);
+        err_x(2,p) = norm(U-u,2)*sqrt(h*k);
         err_x(1,p) = h;
     end
     
    % Check error/corvergance rate in m-direction
 
-    m = 120;
+    m = 200;
     for p = 1:8
         n= 2^p;
         h = 1/(m+1);
@@ -35,7 +37,7 @@ function [err_x, err_y] = error(f,analytic)
         fval = reshape(f(X,Y),m*n,1);
         u = reshape(analytic(X,Y),m*n,1);
         U = fivepoint(fval,m,n);
-        err_y(2,p) = norm(u-U,2)*sqrt(h*k);
+        err_y(2,p) = norm(U-u,2)*sqrt(h*k);
         err_y(1,p) = k;
     end
 end
