38d472b45b2c55a80716006768ef150799affe1c
Mira <miralilleholtvik@gmail.com>
2018-03-20 18:26:15 +0100

    lkj

--- error.m
+++ error.m
@@ -1,12 +1,11 @@
 function [err_x, err_y] = error(f,analytic)
-    n = 300;
-    
-    err_x = zeros(2,8);
-    err_y = zeros(2,8);
-    
+    n = 100;
+    n_points = 8;
+    err_x = zeros(2,n_points);
+    err_y = zeros(2,n_points);
    
     % Check error/corvergance rate in m-direction
-    for p =1:8    
+    for p =1:n_points    
         m= 2^p;
         h = 1/(m+1);
         k = 1/(n+1);
@@ -17,15 +16,16 @@ function [err_x, err_y] = error(f,analytic)
         
         fval = reshape(f(X,Y),m*n,1);
         u = reshape(analytic(X,Y),m*n,1);
-        U = fivepoint(fval,m,n);
+        V = fivepoint(fval,m,n);
+        U = fivepoint(V,m,n);
         err_x(2,p) = norm(U-u,2)*sqrt(h*k);
         err_x(1,p) = h;
     end
     
-   % Check error/corvergance rate in m-direction
+   % Check error/corvergance rate in n-direction
 
-    m = 200;
-    for p = 1:8
+    m = 100;
+    for p = 1:n_points
         n= 2^p;
         h = 1/(m+1);
         k = 1/(n+1);
@@ -36,7 +36,9 @@ function [err_x, err_y] = error(f,analytic)
         
         fval = reshape(f(X,Y),m*n,1);
         u = reshape(analytic(X,Y),m*n,1);
-        U = fivepoint(fval,m,n);
+        V = fivepoint(fval,m,n);
+        U = fivepoint(V,m,n);
+        
         err_y(2,p) = norm(U-u,2)*sqrt(h*k);
         err_y(1,p) = k;
     end
