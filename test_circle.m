b25112b47bb806adeb8f2c7272346fb3b19c564b
sivertsel <18726460+sivertsel@users.noreply.github.com>
2018-03-21 18:34:24 +0100

    typo testcirc

--- test_circle.m
+++ test_circle.m
@@ -1,8 +1,5 @@
-f =@(r,theta) 15*(1-r).*(r.*(1-r).^4 - 49*r.*(1-r).^3 + 124.*r.^2.*(1-r).^2 - 72*r.^3.*(1-r) + 8*r.^4).*cos(theta);
+f =@(r,theta) 15*(1-r).*(3*(1-r).^4 - 49*r.*(1-r).^3 + 124.*r.^2.*(1-r).^2 - 72*r.^3.*(1-r) + 8*r.^4).*cos(theta);
 u = @(r,theta) r.^4.*(1-r).*sin(3*theta).*cos(pi*r/2).*sin(pi*r);
-
-
-
 
 %test plotting
 % M = 4;
@@ -21,6 +18,7 @@ k = 2*pi/N;
 %construct the matrix fval with f(r,theta) in all gridpoints
 r = linspace(h,1-h,M);
 theta = k*(0:(N-1));
+
 [R,THETA] = meshgrid(r,theta);
 F = f(R,THETA)';%need to take the transpose in order to get it row-wise, since default reshape is by column
 %reshape to vector-form, [f(row_1),f(row_2,...,f(row_M)]
@@ -32,7 +30,7 @@ V =  circle(fval,M,N);
 U = circle(V,M,N);
 
 U_matrix = reshape(U,M,N)';
-%må finne korresponderende x og y-verdier
+%mï¿½ finne korresponderende x og y-verdier
 X =R.*cos(THETA);
 Y = R.*sin(THETA);
 
