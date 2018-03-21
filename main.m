d8afeb6e9679c529c9e9e6c475ca48bb41a234a9
sivertsel <18726460+sivertsel@users.noreply.github.com>
2018-03-21 15:22:37 +0100

    written ninepoint into main

--- main.m
+++ main.m
@@ -10,44 +10,42 @@ u_exact = @(x,y) sin(2*pi*x).*sin(2*pi*y)/(64*pi^4);
 
 %% 
 
-% M = 10; %number of internal nodes in x-dir
-% N = 20; %number of internal nodes in y-dir
-% h = 1/(M+1);
-% k = 1/(N+1);
-% %construct the matrix fval with f(x,y) in all gridpoints
-% x = linspace(h,1-h,M);
-% y = linspace(k,1-k,N);
-% [X,Y] = meshgrid(x,y);
-% F = f(X,Y)';%need to take the transpose in order to get it row-wise, since default reshape is by column
-% %reshape to vector-form, [f(row_1),f(row_2,...,f(row_M)]
-% fval = reshape(F,M*N,1);
-% 
-% %eq 1: solve grad^2 v = f
-% V =  fivepoint(fval,M,N);
-% %eq 2: solve grad^2 u = v
-% U = fivepoint(V,M,N);
-% 
-% %feilsøker hvorfor det ikke funker med M!=N
-% %må være noe med hvordan vi tar inn F?
-% 
-% U_matrix = reshape(U,M,N)';
-% u_exact_matrix = u_exact(X,Y);
-% figure(1)
-% plot3(X,Y,U_matrix,'b*',X,Y,u_exact_matrix,'ro')
-% figure(2)
-% plot3(X,Y,U_matrix-u_exact_matrix)
+M = 50; %number of internal nodes in x-dir
+N = 50; %number of internal nodes in y-dir
+h = 1/(M+1);
+k = 1/(N+1);
 
+%construct the matrix fval with f(x,y) in all gridpoints
+x = linspace(h,1-h,M);
+y = linspace(k,1-k,N);
+[X,Y] = meshgrid(x,y);
+F = f(X,Y)';%need to take the transpose in order to get it row-wise, since default reshape is by column
+%reshape to vector-form, [f(row_1),f(row_2,...,f(row_M)]
+fval = reshape(F,M*N,1);
 
+%% Solve %%
 
+%V =  fivepoint(fval,M,N);
+V = ninepoint(fval,M,N); 
+%U = fivepoint(V,M,N);
+U = ninepoint(V,M,N);
 
+%%
+
+%feilsï¿½ker hvorfor det ikke funker med M!=N
+%mï¿½ vï¿½re noe med hvordan vi tar inn F?
+
+U_matrix = reshape(U,M,N)';
+u_exact_matrix = u_exact(X,Y);
+figure(1)
+plot3(X,Y,U_matrix,'b*',X,Y,u_exact_matrix,'ro')
+
+figure(2)
+plot3(X,Y,U_matrix-u_exact_matrix)
 
 
 %% Compute solution error in 2-norm, a.f.o. steplength %%
 [errorx, errory] = error(f,u_exact);
-
-
-errorx
-errory
 
 %% Plot & Business %%
 figure(4)
@@ -59,18 +57,6 @@ grid on
 xlabel('h/k')
 ylabel('2-norm error x-direction')
 title('loglog base 10 on x')
-
-% 
-% plt.loglog(errory[0], errory[1],basex=10)
-% plt.grid(True)
-% plt.xlabel('k')
-% plt.ylabel('2-norm error y-direction')
-% plt.title('loglog base 10 on x')
-% 
-% fig.savefig('solution.pdf')
-% plt.savefig('convergence.pdf')
-
-% 
 
 
 
